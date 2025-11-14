9a21b32ef584548ec3b37d75cc26132040943fcb
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-14 10:06:16 -0500

    added global variables for inputs and error messages

---
+++ script.js
@@ -0,0 +1,12 @@
+// define global variables for html elements
+let registrationForm = document.getElementById('registrationForm');
+let usernameInput = document.getElementById('username');
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
+let confirmPasswordInput = document.getElementById('confirmPassword');
+
+// define global variables for error messages
+let usernameError = document.getElementById('usernameError');
+let emailError = document.getElementById('emailError');
+let passwordError = document.getElementById('passwordError');
+let confirmPasswordError = document.getElementById('confirmPasswordError');
