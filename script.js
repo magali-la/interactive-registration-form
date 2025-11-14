9dda57da67666182b1d1e354acec45a9f74a5ece
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-14 12:22:29 -0500

    added focus event listener to only apply when input is focused, added class toggles for focus and non focus states, added username input validation

--- script.js
+++ script.js
@@ -10,3 +10,55 @@ let usernameError = document.getElementById('usernameError');
 let emailError = document.getElementById('emailError');
 let passwordError = document.getElementById('passwordError');
 let confirmPasswordError = document.getElementById('confirmPasswordError');
+
+// event listener if the input is clicked
+let usernameClicked = false;
+
+usernameInput.addEventListener("focus", () => {
+    usernameClicked = true;
+})
+
+// event listener for username input field
+usernameInput.addEventListener("input", (event) => {
+    // run everything only if the username has focus
+    if (usernameClicked === true) {
+        if (usernameInput.validity.valueMissing){
+            // add the invalid class and remove valid
+            usernameInput.classList.add('invalid');
+            usernameInput.classList.remove('valid');
+            usernameError.innerText = "Username required";
+        } else if (usernameInput.value.length < 8){
+            usernameInput.classList.add('invalid');
+            usernameInput.classList.remove('valid');
+            usernameError.innerText = "Username too short";
+        } else {
+            usernameInput.classList.add('valid');
+            usernameInput.classList.remove('invalid');
+            usernameError.innerText = '';
+        }
+    } else {
+        // if it's false exit the event listener
+        return;
+    }
+});
+
+// add blur event listener so user is alerted when they move from the input field without a valid username 
+usernameInput.addEventListener("blur", (event) => {
+    if (usernameClicked === true) {
+        if (usernameInput.validity.valueMissing){
+            usernameError.innerText = "Username required";
+            usernameInput.classList.add('invalid');
+            usernameInput.classList.remove('valid');
+        } else if (usernameInput.value.length < 8){
+            usernameError.innerText = "Username too short";
+            usernameInput.classList.add('invalid');
+            usernameInput.classList.remove('valid');
+        } else {
+            usernameError.innerText = "";
+            usernameInput.classList.add('valid');
+            usernameInput.classList.remove('invalid');
+        } 
+    } else {
+        return;
+    }
+});
